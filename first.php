7dafaca37915c132b4cda7c735869df99b026b12
Avnish Kumar <vanish.kumar@vichara.com>
2025-02-11 19:52:09 +0530

    comment something in first file

--- first.php
+++ first.php
@@ -1,3 +1,3 @@
 <?php echo "Hello World"; ?>
 
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEKoeJ6if0U/Pz1tcqs8s97dXSZgfJOqdNkfyH0fQw3X singh@Sushant
+<?php //ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEKoeJ6if0U/Pz1tcqs8s97dXSZgfJOqdNkfyH0fQw3X singh@Sushant ?>
